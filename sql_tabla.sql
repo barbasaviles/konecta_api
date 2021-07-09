705a02dbef4b128675adaa6156beea91569993cc
barbas <ing.gabriel.aviles@gmail.com>
2021-07-09 16:04:16 -0500

    tabla de ventas Sql

--- sql_tabla.sql
+++ sql_tabla.sql
@@ -10,3 +10,11 @@ CREATE TABLE `productos` (
   `peso` int(11) NOT NULL,
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Tabla Inventario de productos'
+
+
+CREATE TABLE `ventas` (
+  `id` int(11) DEFAULT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabla de registro de ventas'
